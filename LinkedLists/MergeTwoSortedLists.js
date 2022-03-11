6f8f947fd34d80bb8880be45f8c8ab50496ddd18
steveliu22 <77029560+steveliu22@users.noreply.github.com>
2022-03-11 00:36:22 -0500

    finised max bin tree and merging two sorted lists

---
+++ LinkedLists/MergeTwoSortedLists.js
@@ -0,0 +1,21 @@
+function ListNode(val, next) {
+	this.val = val === undefined ? 0 : val;
+	this.next = next === undefined ? null : next;
+}
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+	if (list1 == null) {
+		return list2;
+	} else if (list2 == null) {
+		return list1;
+	} else if (list1.val > list2.val) {
+		return ListNode(list2.val, mergeTwoLists(list1, list2.next));
+	} else {
+		return ListNode(list1.val, mergeTwoLists(list1.next, list2));
+	}
+};
