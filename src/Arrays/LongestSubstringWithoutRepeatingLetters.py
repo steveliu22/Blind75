12d34c24d5cb4e797155bd657f68efe53ab629b1
steveliu22 <77029560+steveliu22@users.noreply.github.com>
2022-03-11 00:15:51 -0500

    finished Longest substring without repeating letters

---
+++ src/Arrays/LongestSubstringWithoutRepeatingLetters.py
@@ -0,0 +1,24 @@
+
+
+
+class Solution:
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left_index : int = 0
+        seen : dict = {}
+        max_length : int = 0
+
+        for i in range(len(s)):
+            char : str = s[i]
+            if seen.get(char) != None:
+                if not seen.get(char) + 1 < left_index :
+                    left_index = seen.get(char) + 1
+            seen[char] = i
+            length = 1 + i - left_index
+            if length > max_length:
+                max_length = length
+
+        return max_length
+    
+    
+print(Solution.lengthOfLongestSubstring(Solution(), "abba"))
