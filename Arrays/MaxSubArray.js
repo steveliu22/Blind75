615ddc28b201c03f6eee0d565946f174a9252c74
steveliu22 <77029560+steveliu22@users.noreply.github.com>
2022-03-12 00:57:37 -0500

    added comment explanations for maxsubarray and longestsubstring

--- Arrays/MaxSubArray.js
+++ Arrays/MaxSubArray.js
@@ -2,14 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+//utilizes the sliding window pattern
 var maxSubArray = function (nums) {
 	let length = nums.length;
 
 	let leftIndex = 0;
-	let prefixSum = nums[0];
+	let prefixSum = nums[0]; // we are keeping track of the sum that we've accumulated so far in our sub array
 	let maxSum = nums[0];
 
 	for (let i = 1; i < length; i += 1) {
+		//if the sum we've accumulated so far is negative, then it will only decrease the value of our sub array
+		//so we have to get rid of all of the numbers from leftIndex to i - 1 and start over with a new sub array so
+		//we then slide the left most point to our current index
 		if (prefixSum < 0) {
 			leftIndex = i;
 			prefixSum = nums[i];
--- Strings/LongestSubstringWithoutRepeatingLetters.py
+++ Strings/LongestSubstringWithoutRepeatingLetters.py
@@ -1,14 +1,18 @@
 class Solution:
 
+    #solution to this problem requires the sliding window pattern
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left_index : int = 0
-        seen : dict = {}
-        max_length : int = 0
+        left_index : int = 0 #starting point of the longest substring
+        seen : dict = {} #to keep track of the all of the letters we've seen so far
+        max_length : int = 0 #keeps track of the current maximum substring length
 
         for i in range(len(s)):
             char : str = s[i]
-            if seen.get(char) != None:
-                if not seen.get(char) + 1 < left_index :
+            if seen.get(char) != None: #this means that the current character has been repeated
+                #if the character has been repeated but it moves the left index back then we run into the risk of 
+                # including other characters that may also have been repeated (ex: dbvxabd, we see that d is repeated at index 6 but if we were to move the left index
+                # to the first time the d occurs (at index 1), then we include the b which also has been repeated)
+                if seen.get(char) + 1 > left_index : 
                     left_index = seen.get(char) + 1
             seen[char] = i
             length = 1 + i - left_index
